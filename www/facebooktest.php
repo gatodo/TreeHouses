90d50f5b3d15679664228a731b9c8115caff8e3a
Matt McCarthy <mattmc318@gmail.com>
2014-03-30 09:37:26 -0500

    something of a homepage

--- www/facebooktest.php
+++ www/facebooktest.php
@@ -13,7 +13,7 @@
 Below we include the Login Button social plugin. This button uses the JavaScript SDK to
 present a graphical Login button that triggers the FB.login() function when clicked. -->
 
-Your Facebook id is: <output name="id"></output>
+Your Facebook id is: <output id="idout" name="id"></output>
 
 <fb:login-button show-faces="true" width="200" max-rows="1"></fb:login-button>
 

ba1f9125f8b8faf4d068348823b6c4b2adce45ab
Joshua Maravelias <jdmaravelias@gmail.com>
2014-03-30 09:23:22 -0500

    .htaccess
    created a .htaccess to disallow viewing of php scripts

--- www/index.php
+++ www/index.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Tr33Haus - A social network for houses and families of choice</title>
-<link rel="shortcut icon" href="./favicon.ico" />
+  <title>Tr33Haus</title>
+  <link rel="stylesheet" type="text/css" href="resources/style.css" />
+  <link rel="stylesheet" type="text/css" href="resources/layout.css" />
 </head>
-
-<body>
-<h1>Tr33Haus</h1>
-<p>TreeHouses will act as a social network catering to the specific needs and micro-political community structure of the Trans community.</p>
-<p>Check out our <a href="https://github.com/gatodo/TreeHouses">github!</a></p>
+<body class="blue">
+  <div id="titlebar_wrapper" class="brown">
+    <div id="titlebar" class="center">
+      <img src="resources/TH_logo.png" id="logo" alt="Tr33Haus Logo" />
+    </div>
+  </div>
+  <div id="content">
+    <div id="page" class="center">
+      <p>
+        <em>&ldquo;Tr33Haus will act as a social network catering to the specific needs and micro-political community structure of the Trans community.&rdquo;</em>
+      </p>
+      <a>Login with Facebook</a>
+      <a>Create an Account</a>
+    </div>
+    <div id="footer_wrapper" class="gray">
+      <div id="footer" class="center">
+        <a href="https://github.com/gatodo/TreeHouses">GitHub</a>
+        <a>About</a>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
